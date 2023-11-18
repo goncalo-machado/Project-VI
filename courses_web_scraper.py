cdd93c5685596f1631a036c94a1efd28324a1d16
Gonçalo Machado <goncalofmachado01@gmail.com>
2023-11-18 17:14:56 +0000

    temp commit

--- courses_web_scraper.py
+++ courses_web_scraper.py
@@ -21,32 +21,38 @@ def scrap_page(url, w, year, university, wanted_courses):
 
     tables = document.find_all('table', class_='caixa')
 
-    table = tables[1]
+    try:
+        table = tables[1]
+    except:
+        return
 
     courses = []
 
     for td in table.find_all('tr'):
-        data = list(td.stripped_strings)
-        if data[0] not in wanted_courses:
+        try:
+            data = list(td.stripped_strings)
+            if data[0] not in wanted_courses:
+                continue
+            course = {}
+            course['Course Code'] = data[0]
+            course['Course Name'] = data[1]
+            course['Course Type'] = data[2]
+            course['Last Placed Candidate Grade'] = float(data[3].replace(',','.'))
+            course['University Code'] = university
+            course['Year'] = year
+            courses.append(course)
+        except:
             continue
-        course = {}
-        course['Course Code'] = data[0]
-        course['Course Name'] = data[1]
-        course['Course Type'] = data[2]
-        course['Last Placed Candidate Grade'] = float(data[3].replace(',','.'))
-        course['University Code'] = university
-        course['Year'] = year
-        courses.append(course)
         
     w.writerows(courses)
 
 def url_maker(year,university_code):
     return "https://dges.gov.pt/coloc/" + year + "/col1listamin.asp?CodR=11&CodEstab=" + university_code
 
-universities = ['0300']
-courses = ['L221', 'L223', 'L202', 'L217', 'L209', '9119']
+universities = ['0300', '6800', '0203', '0400', '0501','0602','1503','1307','1000','0903','1203','3023','3043','3053','3065','3064','3092','3102','3122','3138','3135','3152','3242','3163','3182','2910','2920','4375','2100','1350','2750','2410','2500','4602','4442','4530','4640','4572']
+courses = ['9119']
 years = ['2021','2022','2023']
-filename = 'Dataset_DETI_Courses_2021_2023.csv'
+filename = 'Dataset_LEI_2021_2023.csv'
 
 with open(filename, 'w', newline='',encoding='utf-8-sig') as f: 
         w = csv.DictWriter(f,['Course Code','Course Name','Course Type','Last Placed Candidate Grade', 'University Code', 'Year']) 
